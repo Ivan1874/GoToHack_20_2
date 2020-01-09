afd80467e9072cd3d2b86c98a1ad2a44b68ec704
allone00 <raldugin06@mail.ru>
2020-01-09 16:56:30 +0400

    I'm car

--- app/routes.py
+++ app/routes.py
@@ -1,22 +1,45 @@
+from tempfile import mktemp
+
+from flask import render_template, send_from_directory, redirect, send_file
+
 from app import app
-from flask import render_template, send_from_directory
+from app.forms import InputForm, Mode
+from .convert import zipdir
 
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
+def main():
+    form = Mode()
+    mode = Mode().mode.data
+    if form.validate_on_submit():
+        return redirect('/load')
+    return render_template('home_page.html', title='Hurt your friend!', form=form)
+
+@app.route('/image/<emotion>')
+def get_emotion(emotion):
+    return send_file(f'C:\\Users\\Александр\\PycharmProjects\\GoToHack_20_2\\tmp\\images\\{emotion}.png', cache_timeout=0)
+
+
+@app.route('/load', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html')
+    form = InputForm()
+    if form.validate_on_submit():
+        filename = mktemp()
+        form.images.data.save(filename)
+        zipdir(filename, 'images')
+        filename2 = mktemp()
+        form.audios.data.save(filename2)
+        zipdir(filename2, 'audios')
+        return redirect('/game')
+    else:
+        return render_template('index.html', title="Hurt your friend", form=form, image='happy')
 
 
-@app.route('/2')
-def index2():
-    return render_template('index2.html')
+@app.route('/game')
+def game():
+    return render_template('game.html')
 
 
-@app.route('/3')
-def index3():
-    return render_template('index3.html')
-
-
-@app.route('/static/<path:path>')
+@app.route('/static/<path>')
 def send_static(path):
     return send_from_directory('static', path)
